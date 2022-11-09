2df6fc20387fdb7ec8b66625c2b7e00a98c89b6e
YuriyRysak <yuriy.risak@gmail.com>
2022-11-09 23:16:54 +0200

    9-11
    ++

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -13,44 +13,15 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const imagesContainer = document.querySelector('.gallery');
-const imagesMarkup = createItemsGalleryMarkup(galleryItems);
+const imagesMarkup = galleryItems
+.map(({ preview, description, original }) =>
+	`<a class="gallery__item" href="${original}">
+		<img class="gallery__image" src="${preview}" alt="${description}" />
+	</a>`
+	).join('');
 
 imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
-imagesContainer.addEventListener('click', onImageContainerClick)
 
-function createItemsGalleryMarkup (galleryItems) {
-    return galleryItems.map(({preview, original, description}) => {
-        return `
-                <a class="gallery__item" href="large-image.jpg">
-                    <img class="gallery__image"
-                    src="${preview}" 
-                    alt="${description}" />
-                </a>
-                `;
-            })
-            .join(''); 
-     
-   
-}
+new SimpleLightbox('.gallery a', { captionsData:"alt",captionDelay: 250, captionPosition: "bottom",});
 
-// const gallery = document.querySelector('.gallery')
-// const items = []
 
-// galleryItems.forEach(element => {
-// 	const galleryLink = document.createElement('a')
-// 	galleryLink.className = 'gallery__link'
-// 	galleryLink.href = element.original
-// 	const galleryImage = document.createElement('img')
-// 	galleryImage.className = 'gallery__image'
-// 	galleryImage.src = element.preview
-// 	galleryImage.setAttribute('title', element.description)
-// 	galleryImage.alt = element.description
-
-// 	galleryLink.append(galleryImage)
-// 	items.push(galleryLink)
-// })
-// gallery.append(...items)
-
-new SimpleLightbox('.gallery a', {
-	captionDelay: 250
-})
