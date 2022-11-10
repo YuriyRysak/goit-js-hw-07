41ac17bcfb474997a4ebaf9d6bf58f3b63781c71
YuriyRysak <yuriy.risak@gmail.com>
2022-11-10 12:44:13 +0200

    II-variant

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -25,3 +25,27 @@ imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
 new SimpleLightbox('.gallery a', { captionsData:"alt",captionDelay: 250, captionPosition: "bottom",});
 
 
+
+// II variant
+
+// const gallery = document.querySelector('.gallery')
+// const items = []
+
+// galleryItems.forEach(element => {
+// 	const galleryLink = document.createElement('a')
+// 	galleryLink.className = 'gallery__link'
+// 	galleryLink.href = element.original
+// 	const galleryImage = document.createElement('img')
+// 	galleryImage.className = 'gallery__image'
+// 	galleryImage.src = element.preview
+// 	galleryImage.setAttribute('title', element.description)
+// 	galleryImage.alt = element.description
+
+// 	galleryLink.append(galleryImage)
+// 	items.push(galleryLink)
+// })
+// gallery.append(...items)
+
+// new SimpleLightbox('.gallery a', {
+// 	captionDelay: 250
+// })
